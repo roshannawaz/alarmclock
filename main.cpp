e3578fe857695b489401058f39b81b4e65adedbf
Roshan Nawaz <84237046+roshannawaz@users.noreply.github.com>
2025-01-10 23:21:48 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,29 +29,3 @@ void setAlarm(int alarmHour, int alarmMinute) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
-
-int main() {
-    int hour, minute;
-
-    std::cout << "Set the alarm time." << std::endl;
-    std::cout << "Hour (0-23): ";
-    std::cin >> hour;
-    std::cout << "Minute (0-59): ";
-    std::cin >> minute;
-
-    if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
-        std::cerr << "Invalid time entered. Please run the program again with valid inputs." << std::endl;
-        return 1;
-    }
-
-    std::cout << "Alarm is set for " << std::setw(2) << std::setfill('0') << hour << ":"
-              << std::setw(2) << std::setfill('0') << minute << ".\n";
-
-    std::thread displayThread(displayCurrentTime);
-
-    setAlarm(hour, minute);
-
-    displayThread.detach();
-
-    return 0;
-}
